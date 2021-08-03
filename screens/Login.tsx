9aa017242be94320e46645673f6b4d0faa4874df
kitani islam <slmkitani@gmail.com>
2021-08-03 01:25:28 +0100

    :sparkles: LoginPage

---
+++ screens/Login.tsx
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import { useEffect } from 'react'
+import { Ionicons } from '@expo/vector-icons';
+import { View, StyleSheet } from 'react-native';
+import { List, InputItem, WhiteSpace, Button } from 'antd-mobile-rn'
+
+
+export default function Login() {
+  useEffect(() => {
+    console.log(InputItem);
+  }, [])
+  return (
+    <View style={styles.container}>
+      <List style={styles.loginItems}>
+        <InputItem
+          placeholder="Username"
+          type="text"
+        >
+           <Ionicons name="person-outline" style={{}} size={18} color="black" />
+        </InputItem>
+      </List>
+      <WhiteSpace  size="md"/>
+      <List style={styles.loginItems}>
+        <InputItem
+          placeholder="Password"
+          type="password"
+        >
+           <Ionicons name="ios-lock-closed-outline" style={{}} size={18} color="black" />
+        </InputItem>
+      </List>
+      <WhiteSpace  size="md"/>
+      <Button type="primary" style={styles.loginItems}>
+        Login
+      </Button>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  loginItems: {
+    marginHorizontal: 10
+  }
+});
