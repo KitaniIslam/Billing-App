c5cd2f43baf3f6fa2653611151ce632f24618761
kitani islam <slmkitani@gmail.com>
2021-08-03 17:40:12 +0100

    :sparkles: ProductDetails Page

--- screens/ProductDetails.tsx
+++ screens/ProductDetails.tsx
@@ -1,19 +1,44 @@
 import * as React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, StyleSheet } from 'react-native'
+import { List } from 'antd-mobile-rn'
+const Item = List.Item
+const Brief = Item.Brief
 export default function ProductDetails({ route }: { route: any }) {
-
   return (
-    <View>
-      <Text>{
-      route.params.name + ' ' +
-      route.params.quantity + ' '+
-      route.params.unitPrice + ' '
-      + ' ' + route.params.totalPrice + ' '
-      + route.params.tax + ' ' +
-      route.params.discount
-      }</Text>
+    <View style={styles.wrapper}>
+      <View style={styles.productInfo}>
+        <Text style={styles.productName}>{route.params.name}</Text>
+        <Text>{route.params.unitPrice} $</Text>
+      </View>
+      <List>
+        <Item>Quantity <Brief>{route.params.quantity}</Brief></Item>
+        <Item>Discount <Brief>- {route.params.discount} %</Brief></Item>
+        <Item>Tax <Brief>+ {route.params.tax} %</Brief></Item>
+        <Item extra={route.params.totalPrice+' $'}>Total Price <Brief>With Tax</Brief></Item>
+      </List>
       
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    display: 'flex',
+    flex: 1,
+    backgroundColor: '#ffffff'
+  },
+  productInfo: {
+    display: 'flex',
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    backgroundColor: '#ebeef0',
+    color: '#ffffff'
+  },
+  productName: {
+    fontSize: 25,
+    fontWeight: 'bold'
+  }
+})
