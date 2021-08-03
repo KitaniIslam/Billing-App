71830de6de0eb479d8b7d08def67d95608165904
kitani islam <slmkitani@gmail.com>
2021-08-04 00:50:11 +0100

    :zap: Billing> import DetailsModal and set inputs for newProduct

--- screens/Billing.tsx
+++ screens/Billing.tsx
@@ -1,23 +1,99 @@
 import * as React from 'react';
-import { View , Text , StyleSheet} from 'react-native';
+import { View, StyleSheet, LogBox, Alert, ScrollView } from 'react-native';
+import DetailsModal from '../components/DetailsModal';
+import { Stepper, List, InputItem, Button, Picker } from 'antd-mobile-rn';
 
 
-export default function Billing () {
+export default function Billing() {
+
+  React.useEffect(() => {
+    LogBox.ignoreLogs(['Animated: `useNativeDriver`']);
+  }, [])
+
+  const [selectedState, setState] = React.useState(["AL"])
+  const changeState = () => {
+    Alert.alert('Change State Error', 'State cant be change at the moment, default one : AL')
+  };
+
+  const data = [
+    { label: "AK", value: 5 },
+    { label: "AL", value: 6 },
+    { label: "AR", value: 3 },
+    { label: "AS", value: 5.7 },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Billing Page</Text>
-    </View>
+      <View style={styles.inputArea}>
+        <ScrollView>
+
+        <List>
+          <InputItem
+            clear
+            type="text"
+            onChange={value => { }}
+            placeholder="Enter Product Label"
+          >
+            Product
+          </InputItem>
+          <InputItem
+            clear
+            type="number"
+            onChange={value => { }}
+            placeholder="Enter Price"
+          >
+            Price
+          </InputItem>
+          <List.Item
+            extra={
+              <Stepper
+                key="0"
+                min={1}
+                defaultValue={1}
+              />
+            }
+          >
+            Quantity
+          </List.Item>
+          <Picker
+            data={data}
+            cols={1}
+            value={selectedState}
+            okText="Select"
+            title="Select State"
+            extra={selectedState[0]}
+            dismissText="cancel"
+            onChange={changeState}
+          >
+            <List.Item arrow="down" >
+            Select State
+            </List.Item>
+          </Picker>
+          <Button type="primary">Add To List</Button>
+        </List>
+        </ScrollView>
+      </View>
+      <DetailsModal
+        originalPrice={1000}
+        discount={3}
+        tax={6}
+        totalPrice={1080}
+      />
+    </View >
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  detailsButton: {
+  },
+  inputArea: {
+    display: 'flex',
+    flex: 1
+  },
+  picker: {
+    backgroundColor: '#9f98c5'
   }
 });
