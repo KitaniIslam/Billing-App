408b5f83b3afe6de17245bef891525a8a6848674
kitani islam <slmkitani@gmail.com>
2021-08-05 13:59:05 +0100

    :sparkles: ProductStore> calculate Basket Prices

--- store/futures/Products.ts
+++ store/futures/Products.ts
@@ -4,7 +4,13 @@ import { Product, ProductInBasket, Tax } from '../../types'
 interface globalState {
   Products: Product[];
   Basket: ProductInBasket[];
-  Taxes: Tax[]
+  Taxes: Tax[],
+  totalPriceInBasket: number;
+  taxedTotalPriceInBasket: number;
+  discount: number;
+  tax: number;
+  discountAppliedOnBasket: number;
+  taxAppliedOnBasket: number;
 };
 
 const initialState: globalState = {
@@ -15,7 +21,13 @@ const initialState: globalState = {
     { label: 'AL', value: 6 },
     { label: 'AR', value: 3 },
     { label: 'AS', value: 5.7 },
-  ]
+  ], 
+  totalPriceInBasket: 0,
+  taxedTotalPriceInBasket: 0,
+  discount: 3,
+  tax: 6, 
+  discountAppliedOnBasket: 0,
+  taxAppliedOnBasket: 0,
 };
 
 const Products = createSlice({
@@ -24,7 +36,28 @@ const Products = createSlice({
   reducers: {
     addProduct(state, action: PayloadAction<ProductInBasket>) {
       state.Basket.push(action.payload)
+      Products.caseReducers.calculateBasketItemsPrice(state)
     },
+    calculateBasketItemsPrice(state) {
+      const discount: number[] = []
+      const tax: number[] = []
+      const basket = state.Basket
+      const totalPrice = basket.reduce((reducer, item) => reducer + (item.price * item.quantity) , 0)
+      const taxedTotalPrice = basket.reduce((reducer, item) => {
+        const totalPriceForProduct:number = item.price * item.quantity
+        const totalPriceForProductAfterDiscount:number = totalPriceForProduct - (totalPriceForProduct * (state.discount /100))
+        const taxForRegion:any = state.Taxes.filter((itemToFilter) => itemToFilter.label === item.state)
+        const taxToApply:number = totalPriceForProductAfterDiscount * (taxForRegion[0].value /100)
+        discount.push(totalPriceForProduct * (state.discount /100))
+        tax.push(taxToApply)
+        return (reducer + totalPriceForProductAfterDiscount + taxToApply)
+      }, 0)
+
+      state.discountAppliedOnBasket = discount.reduce((reducer, item) => reducer + item, 0)
+      state.taxAppliedOnBasket = tax.reduce((reducer, item) => reducer + item, 0)
+      state.totalPriceInBasket = totalPrice
+      state.taxedTotalPriceInBasket = taxedTotalPrice
+  },
     deleteProduct(state, action) {},
     calculatePrice(state) {}
   },
